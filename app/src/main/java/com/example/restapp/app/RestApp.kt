d741fe0cdf30a456108a1f984ea2feae651f0c9d
whalive <whalive2104@gmail.com>
2023-01-25 13:48:37 +0500

    Authentication with firebase part 1

--- app/src/main/java/com/example/restapp/app/RestApp.kt
+++ app/src/main/java/com/example/restapp/app/RestApp.kt
@@ -3,17 +3,17 @@ package com.example.restapp.app
 import android.app.Application
 import com.example.restapp.Prefs
 
-class RestApp : Application() {
-
-    val prefs by lazy { Prefs(applicationContext) }
-
-    override fun onCreate() {
-        super.onCreate()
-        INSTANCE = this
-    }
-
-    companion object {
-        lateinit var INSTANCE: RestApp
-            private set
-    }
-}
+//class RestApp : Application() {
+//
+//    val prefs by lazy { Prefs(applicationContext) }
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        INSTANCE = this
+//    }
+//
+//    companion object {
+//        lateinit var INSTANCE: RestApp
+//            private set
+//    }
+//}
