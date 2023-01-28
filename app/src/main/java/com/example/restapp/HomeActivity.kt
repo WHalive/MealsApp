8786e0075ff56910e0c71ef6f83fca8e8a939331
whalive <whalive2104@gmail.com>
2023-01-28 16:07:47 +0500

    Bottom navigation and appbar

--- app/src/main/java/com/example/restapp/HomeActivity.kt
+++ app/src/main/java/com/example/restapp/HomeActivity.kt
@@ -2,10 +2,43 @@ package com.example.restapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.restapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private var _binding: ActivityHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        _binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val homeFragment = HomeFragment()
+        val favouriteFragment = FavouriteFragment()
+        val searchFragment = SearchFragment()
+        val listFragment = ListFragment()
+        val infoFragment = InfoFragment()
+
+        val bottomNavigationView = binding.bottomNav
+        replaceFragment(homeFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> replaceFragment(homeFragment)
+                R.id.favouriteFragment -> replaceFragment(favouriteFragment)
+                R.id.searchFragment -> replaceFragment(searchFragment)
+                R.id.listFragment -> replaceFragment(listFragment)
+                R.id.infoFragment -> replaceFragment(infoFragment)
+            }
+            true
+        }
+        bottomNavigationView.selectedItemId = R.id.homeFragment
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
